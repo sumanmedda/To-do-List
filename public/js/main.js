7058566144db4ace90ba9c6962037ace69b79f9a
sumanmedda <sumanmedda33.com>
2020-04-17 20:48:01 +0530

    redo of todo list

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,34 @@
+const addTodoBtn = $('#add-Todo'); // store add todo button element in variable
+const newTask = $('#new-task'); // store new task input element in variable
+const myTasklist = $('.my-tasks-list'); // store my task list element in variable
+const taskArray = []; // array to store all the task
 
 $(document).ready(() => {
+    addTodoBtn.click(() =>{
 
-    //adding list from text box
-    $('.buttons').click(() => {
-        let mylist = $('#input').val();
-        $('#list').append('<li class="mgt-list" id="hidelist" style="text-align:left;!important">' + mylist + '<button type="button" class="rembtn btn-sm" id="remtxt">-</button></li>');
-        return false;
-    });
-    
-    //changing of text of button
-    $('#btntoggle').click(function(){
-        $(this).toggleClass('btn-text');
-        if ($(this).hasClass('btn-text')) {
-            $(this).text('Show your to-do List')
-        } else {
-            $(this).text('Hide your to-do List')
+        const task = newTask.val(); // store val of newTask input in task variable
+
+        // clear input to avoid duplicate clicks
+        newTask.val('');
+
+        // check input empty
+        if (task == ''){
+            alert('Please add some task');
+            return;
         }
 
-        //toggle of contents of buttons 
-        $( ".textid" ).toggle();
-        $( "#plus" ).toggle();
-        $( ".hidelist" ).toggle();
-    })
-    
+        // check task already there in taskArray
+        // do not add task if in taskArray
+        if(taskArray.includes(task)){
+            alert('Task Already present');
+            return;
+        }
+
+        // add task to task array
+        taskArray.push(task);
+
+        // append task to list
+        myTasklist.append(`<li class="task">${task}</li>`);
+        
+    });
 });
