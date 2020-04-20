63da4328465605bc1eb676891b02049e94ab6105
sumanmedda <sumanmedda33.com>
2020-04-20 12:00:49 +0530

    setting up firebase

--- public/js/main.js
+++ public/js/main.js
@@ -12,69 +12,104 @@
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    
+    $(document).ready(function() {
 
-const addTodoBtn = $('#add-Todo'); // store add todo button element in variable
-const newTask = $('#new-task'); // store new task input element in variable
-const myTasklist = $('.my-tasks-list'); // store my task list element in variable
-const taskArray = []; // array to store all the task
+        $('#add-Todo').on('click', () => {
+        // value of input box
+        const task = $('#new-task').val();
 
-$(document).ready(() => {
-    addTodoBtn.on('click', () =>{
-
-        const task = newTask.val(); // store val of newTask input in task variable
-
-        // clear input to avoid duplicate clicks
-        newTask.val('');
-
-        // check input empty
-        if (task == ''){
-            alert('Please add some task');
-            return;
-        }
-
-        // check task already there in taskArray
-        // do not add task if in taskArray
-        if(taskArray.includes(task)){
-            alert('Task Already present');
-            return;
-        }
-
-        // add task to task array
-        taskArray.push(task);
-
-        // checkbox for showing completed task & added class task-complete-checkbox
-        const checkBox = (`<input type="checkbox" class="task-complete-checkbox"/>`);
-
-        // button for removing task from list & added class task-complete-button
-        const remBtn = (`<button class="task-complete-button"><i class="fa fa-trash-o"></i></button>`);
-
-        // append task to list
-        myTasklist.append(`<li class="task">${checkBox}<span class="span-text">${task}</span>${remBtn}<hr></li>`);
+        // storing database in variable database
+        let database = firebase.database();
         
-    });
+        // store value of key into a variable
+        let key = database.ref().child("unfinished_task").push().key;
 
-    // button is dynamically generated so we are using delegated event listner 'on'
-    // arrow function do not send the clicked element 
-    // always use normal function with delegated event 
-    $('.my-tasks-list').on('click','.task-complete-button', function()  {
+        // enter and updating in real time database
+        if(task != 0){
+            key;
+            let taskList = {
+                task: task,
+                key : key
+            };
 
-        //  we have to remove the parent li
-        const task = $(this).parent().find('span').html();
+            let updates = {};
+            updates["/unfinished_task/" + key] = taskList;
+            database.ref().update(updates);
+   
+        };
 
-        // delete task from taskArray
-        const index = taskArray.indexOf(task);
-        delete taskArray[index];
-
-        // remove the task by deleting the parent li
-        $(this).parent().remove();
+        // after input clear data
+            $('#new-task').val('');
     
     });
-
-    $('.my-tasks-list').on('click','.task-complete-checkbox', function() {
-
-        // find the value of span
-        const taskSpan = $(this).parent().find('span');
-    });
-
-
 });
+
+
+
+
+    // const addTodoBtn = $('#add-Todo'); // store add todo button element in variable
+    // const newTask = $('#new-task'); // store new task input element in variable
+    // const myTasklist = $('.my-tasks-list'); // store my task list element in variable
+    // const taskArray = []; // array to store all the task
+
+    // $(document).ready(() => {
+    //     addTodoBtn.on('click', () =>{
+
+    //         const task = newTask.val(); // store val of newTask input in task variable
+
+    //         // clear input to avoid duplicate clicks
+    //         newTask.val('');
+
+    //         // check input empty
+    //         if (task == ''){
+    //             alert('Please add some task');
+    //             return;
+    //         }
+
+    //         // check task already there in taskArray
+    //         // do not add task if in taskArray
+    //         if(taskArray.includes(task)){
+    //             alert('Task Already present');
+    //             return;
+    //         }
+
+    //         // add task to task array
+    //         taskArray.push(task);
+
+    //         // checkbox for showing completed task & added class task-complete-checkbox
+    //         const checkBox = (`<input type="checkbox" class="task-complete-checkbox"/>`);
+
+    //         // button for removing task from list & added class task-complete-button
+    //         const remBtn = (`<button class="task-complete-button"><i class="fa fa-trash-o"></i></button>`);
+
+    //         // append task to list
+    //         myTasklist.append(`<li class="task">${checkBox}<span class="span-text">${task}</span>${remBtn}<hr></li>`);
+            
+    //     });
+
+    //     // button is dynamically generated so we are using delegated event listner 'on'
+    //     // arrow function do not send the clicked element 
+    //     // always use normal function with delegated event 
+    //     $('.my-tasks-list').on('click','.task-complete-button', function()  {
+
+    //         //  we have to remove the parent li
+    //         const task = $(this).parent().find('span').html();
+
+    //         // delete task from taskArray
+    //         const index = taskArray.indexOf(task);
+    //         delete taskArray[index];
+
+    //         // remove the task by deleting the parent li
+    //         $(this).parent().remove();
+        
+    //     });
+
+    //     $('.my-tasks-list').on('click','.task-complete-checkbox', function() {
+
+    //         // find the value of span
+    //         const taskSpan = $(this).parent().find('span');
+    //     });
+
+
+    // });
