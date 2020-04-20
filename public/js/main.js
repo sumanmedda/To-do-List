87f042187536c0eadbc6303114d29944dfe29861
sumanmedda <sumanmedda33.com>
2020-04-20 10:45:58 +0530

    setting up firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,17 @@
+// Firebase integration
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDRkDB6cuID5hh6SWFdg0-ZCYsbuxLz-nk",
+        authDomain: "to-do-list-d70a0.firebaseapp.com",
+        databaseURL: "https://to-do-list-d70a0.firebaseio.com",
+        projectId: "to-do-list-d70a0",
+        storageBucket: "to-do-list-d70a0.appspot.com",
+        messagingSenderId: "452500715828",
+        appId: "1:452500715828:web:42066c1e5d5af340ca8d61"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
 const addTodoBtn = $('#add-Todo'); // store add todo button element in variable
 const newTask = $('#new-task'); // store new task input element in variable
 const myTasklist = $('.my-tasks-list'); // store my task list element in variable
@@ -31,10 +45,10 @@ $(document).ready(() => {
         const checkBox = (`<input type="checkbox" class="task-complete-checkbox"/>`);
 
         // button for removing task from list & added class task-complete-button
-        const remBtn = (`<button class="task-complete-button">-</button>`);
+        const remBtn = (`<button class="task-complete-button"><i class="fa fa-trash-o"></i></button>`);
 
         // append task to list
-        myTasklist.append(`<li class="task">${checkBox}<span>${task}</span>${remBtn}</li>`);
+        myTasklist.append(`<li class="task">${checkBox}<span class="span-text">${task}</span>${remBtn}<hr></li>`);
         
     });
 
@@ -51,17 +65,14 @@ $(document).ready(() => {
         delete taskArray[index];
 
         // remove the task by deleting the parent li
-        console.log(taskArray);
         $(this).parent().remove();
     
     });
 
     $('.my-tasks-list').on('click','.task-complete-checkbox', function() {
 
+        // find the value of span
         const taskSpan = $(this).parent().find('span');
-
-        //
-        console.log($(this).parent().find('span').html());
     });
 
 
